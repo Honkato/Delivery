285a563c715c8324a2299b804bf4d84bffe77d46
Honkato <77444910+Honkato@users.noreply.github.com>
2023-06-30 14:26:06 -0300

    Update User.java

--- DeliveryenceOfSouls/src/entities/User.java
+++ DeliveryenceOfSouls/src/entities/User.java
@@ -1,11 +1,40 @@
 package entities;
 
 public class User {
-    int id;
-    String name;
-    int x;
-    int y;
-    String CPF;
+    private final int id;
+    private String name;
+    private int x;
+    private int y;
+    private final String CPF;
+    private Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
@@ -26,15 +55,23 @@ public class User {
         return CPF;
     }
 
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
-
+//    public void setCPF(String CPF) {
+//        this.CPF = CPF;
+//    }
     public User(int id, String name, String CPF, int x, int y) {
         this.id = id;
         this.name = name;
         this.CPF = CPF;
         this.x = x;
         this.y = y;
+        this.role = Role.USER;
+    }
+    public User(int id, String name, String CPF, int x, int y, Role role) {
+        this.id = id;
+        this.name = name;
+        this.CPF = CPF;
+        this.x = x;
+        this.y = y;
+        this.role = role;
     }
 }
