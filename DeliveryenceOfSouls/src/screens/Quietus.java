af0ef9fb6343a49ca2e7a9a87447af54a81fa81e
Honkato <77444910+Honkato@users.noreply.github.com>
2023-06-30 14:27:49 -0300

    Create Quietus.java

---
+++ DeliveryenceOfSouls/src/screens/Quietus.java
@@ -0,0 +1,87 @@
+package screens;
+
+import entities.Pedido;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Quietus extends JPanel {
+    private final DecimalFormat df = new DecimalFormat("#.##");
+    private JScrollPane scrollPaneQuietus = new JScrollPane();
+    private DefaultListModel<String> modelQuietus = new DefaultListModel<>();
+    private DefaultListModel<String> modelQuietusPlaceholder = new DefaultListModel<>();
+    private final JList<String> listaQuietus = new JList<>(modelQuietus);
+
+    public void setModelQuietus(DefaultListModel<String> modelQuietus) {
+        this.modelQuietus = modelQuietus;
+        scrollPaneQuietus.setViewportView(listaQuietus);
+        System.out.println("z");
+        System.out.println(this.modelQuietus);
+        System.out.println("zz");
+        System.out.println(modelQuietus);
+//        System.out.println(this.listaQuietus);
+    }
+
+    public DefaultListModel<String> getModelQuietus() {
+        return modelQuietus;
+    }
+
+    public Quietus(){
+        Border blackline;
+
+        blackline = BorderFactory.createLineBorder(Color.BLACK);
+        TitledBorder borderQuietus;
+
+        borderQuietus = BorderFactory.createTitledBorder(blackline,"Quietus");
+        borderQuietus.setTitleJustification(TitledBorder.CENTER);
+
+        scrollPaneQuietus.setSize(405,400);
+        scrollPaneQuietus.setLocation(40,100);
+        scrollPaneQuietus.setBorder(borderQuietus);
+
+        add(scrollPaneQuietus);
+
+        setBackground(Color.ORANGE);
+        setLayout(null);
+        setVisible(true);
+    }
+    public void refresh(){
+        System.out.println("zzz");
+        String lastName = modelQuietus.get(0).split(" : ")[0];
+        double subTotal = 0;
+        double total = 0;
+        modelQuietusPlaceholder.removeAllElements();
+        modelQuietusPlaceholder.addElement(lastName+":");
+        for (int i = 0; i < modelQuietus.getSize(); i++){
+            String currentName = modelQuietus.get(i).split(" : ")[0];
+            String foodQtdPrice = modelQuietus.get(i).split(" : ")[1];
+
+            int foodQtd = Integer.parseInt(foodQtdPrice.split(",")[1]);
+            float foodPrice = Float.parseFloat(foodQtdPrice.split(",")[2]);
+            if (!Objects.equals(currentName, lastName)){
+                lastName = currentName;
+                System.out.println(currentName);
+                modelQuietusPlaceholder.addElement("subtotal: R$"+ df.format(subTotal));
+                subTotal = 0;
+                modelQuietusPlaceholder.addElement("------------------------------");
+                modelQuietusPlaceholder.addElement(currentName+":");
+            }
+            subTotal += foodQtd * foodPrice;
+            total += foodQtd * foodPrice;
+            modelQuietusPlaceholder.addElement(" -"+foodQtdPrice.replace(",", " - "));
+        }
+        modelQuietusPlaceholder.addElement("subtotal: R$"+ df.format(subTotal));
+        modelQuietusPlaceholder.addElement("------------------------------");
+        modelQuietusPlaceholder.addElement("Total: R$"+df.format(total));
+        listaQuietus.setModel(modelQuietusPlaceholder);
+//        modelQuietus.addElement("aaaaa");
+//        System.out.println(modelQuietus);
+        scrollPaneQuietus.setViewportView(listaQuietus);
+    }
+
+}
